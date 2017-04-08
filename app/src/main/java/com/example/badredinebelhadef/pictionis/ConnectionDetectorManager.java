fadbffdbf89c38138d59fb32e2f535078b2d0d13
badr-dev <badredine.belhadef@epitech.eu>
2017-04-08 17:36:43 +0200

    class Connection Network

---
+++ app/src/main/java/com/example/badredinebelhadef/pictionis/ConnectionDetectorManager.java
@@ -0,0 +1,41 @@
+package com.example.badredinebelhadef.etnaapp;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by badredine belhadef on 06/04/2017
+ */
+
+public class ConnectionDetectorManager {
+
+    public static boolean isConnectingToInternet(Context context) {
+
+        Boolean hasActiveInternet = false;
+
+        ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if(connectivity != null) {
+
+            NetworkInfo activeNetWork = connectivity.getActiveNetworkInfo();
+
+            if(activeNetWork != null) {
+
+                switch (activeNetWork.getType()) {
+
+                    case ConnectivityManager.TYPE_WIFI :
+                        hasActiveInternet = true;
+                        break;
+                    case ConnectivityManager.TYPE_MOBILE:
+                        hasActiveInternet = true;
+                        break;
+                    default:
+                        hasActiveInternet = false;
+                        break;
+                }
+            }
+        }
+        return  hasActiveInternet;
+    }
+}
